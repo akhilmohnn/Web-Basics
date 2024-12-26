d2249858f797b2766b6a3c26eded30f95f60f3b1
Akhil Mohanan <akhilmohnn@outlook.com>
2024-12-27 00:58:54 +0530

    employee input form

---
+++ PHP-Basics/Employee/employeeInput.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+<title>Employee details</title>
+</head>
+
+<body>
+
+<center>
+<h3>EMPLOYEE DETAILS</h3>
+<form name="employeeForm method="POST" action="employeeData.php">
+
+Employee id:<input type="text" id="employee_id" name="employee_id" placeholder="Enter employee id" required>
+<br><br>
+Employee name:<input type="text" id="employee_name" name="employee_name" placeholder="enter name">
+<br><br>
+Employee's salary:<input type="text" id="employee_salary" name="employee_salary">
+<br><br>
+Employee age:<input type="text" id="employee_age" name="employee_age">
+<br>
+<input type="submit" name="submit" value="SAVE">
+
+</form>
+</center>
+</body>
