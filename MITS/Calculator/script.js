12b5f9058032a7cd1257451e5823c67db48c1148
Akhil Mohanan <akhilmohnn@outlook.com>
2025-01-12 00:26:35 +0530

    calculator basic page

---
+++ MITS/Calculator/script.js
@@ -0,0 +1,14 @@
+function clearDisplay(){
+    document.getElementById("screen").value="";
+
+}
+
+function buttonclick(val){
+    document.getElementById("screen").value+=val
+}
+
+function equalClick(){
+    var text=document.getElementById("screen").value;
+    var result=eval(text);
+    document.getElementById("screen").value=result;
+}
